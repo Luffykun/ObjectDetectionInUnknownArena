d637dbb48935a934f695a51d4783af547e9d4d99
Harsh Sheth <harsheth96@gmail.com>
2015-04-19 22:53:39 +0530

    Added xbee communication

--- FireBird_V_Code.c
+++ FireBird_V_Code.c
@@ -1,28 +1,38 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <math.h>
+#include<math.h>
 
 //Initializing global variable
 
 int global_orientatn = 0;
 /*global orientatn specifies the orientatn of the bot in the arena.
 i.e. whether the bot is facing forward or right or left or backward.*/
+int flag_goes_into_interrupt=0;
+int pulkit_ans;
+int net_angle=0;
+unsigned char data=0x00;
 
+int counter;
 int forward = 0;
 //stores the displacement of the bot ONLY in the forward direction.
 int right = 0;
 //stores the displacement of the bot ONLY in the rightward/leftward directn.
-int counter = 0;
-int forward_last = 0;
-int right_last = 0;
-unsigned int rotatn_angle = 90;
+
 unsigned long int ShaftCountLeft = 0;
 //to keep track of left position encoder
 unsigned long int ShaftCountRight = 0;
 //to keep track of right position encoder
 
-double net_angle;
+void uart0_init(void)
+{
+ UCSR0B = 0x00; //disable while setting baud rate
+ UCSR0A = 0x00;
+ UCSR0C = 0x06;
+ UBRR0L = 0x5F; //set baud rate lo
+ UBRR0H = 0x00; //set baud rate hi
+ UCSR0B = 0x98;
+}
 
 void motion_pin_config (void)
 {
@@ -191,6 +201,7 @@ void linear_distance_mm1(unsigned int DistanceInMM)
 
 }
 
+
 //turns left as well as updates the global orientation
 void turn_left(void)
 {
@@ -225,22 +236,6 @@ void turn_left1(void)
 	motion_set(0x00);
 }
 
-//this function scans 360 degrees looking for the specified object
-void rotate_360 (int rotatn_angle)
- {   counter=0;
-	 for (int i = 0 ; i < 360 / rotatn_angle ; i++)
-		 {
-		  motion_set(0x0A);
-		  angle_rotate(rotatn_angle);
-		  motion_set(0x00);
-		  _delay_ms(500);
-		  counter++;
-		  /*check pulkits ans
-		  if yes pura loop break*/
-		 }
- }
-
-
 unsigned char ADC_Conversion(unsigned char Ch)
 {
 	unsigned char a;
@@ -270,6 +265,99 @@ unsigned int Sharp_GP2D12_estimation(unsigned char adc_reading)
 	}
 	return distanceInt;
 }
+
+
+void move_towards_ball()
+{ double proportionality_factor=45.0/64.0;
+  int reqd_angle;
+  unsigned int value;
+  unsigned char sharp;
+  	sharp = ADC_Conversion(11);
+	value = Sharp_GP2D12_estimation(sharp);
+  while(value>=200)
+  {
+
+  flag_goes_into_interrupt=0;
+  reqd_angle = pulkit_ans * proportionality_factor;
+  rotate_to_centre(reqd_angle);
+  linear_distance_mm1(200);
+  update_coordinates(reqd_angle);
+  //_delay_ms(2000);
+
+  send_data(0x00);//send byte to laptop
+  while(!flag_goes_into_interrupt);
+
+  	sharp = ADC_Conversion(11);
+	value = Sharp_GP2D12_estimation(sharp);
+
+  }
+  motion_set(0x00);
+  while(1);
+}
+
+SIGNAL(SIG_USART0_RECV)
+{ 
+  pulkit_ans = 0;
+  data=UDR0;
+  if(data!=0x03)
+   flag_goes_into_interrupt=1;
+  int first_bit = (data & 0x80)/128; //Check first bit to see if ball was found
+  //if(data != 0xFF)
+  //{
+  if(first_bit == 1)
+  {
+ 	int second_bit = (data & 0x40)/64;
+	pulkit_ans = data & 0x3F;
+	if(second_bit == 1)
+	{
+		pulkit_ans *= (-1);
+	}
+  }
+  //}
+}
+
+void send_data(unsigned char send)
+{
+	while(data != 0x03)
+	{
+		UDR0 = send;	
+		//_delay_ms(1000);
+	}
+}
+
+//this function scans 360 degrees looking for the specified object
+void rotate_360 (int rotatn_angle)
+ {	counter=0;
+    int first_bit;
+	 for (int i = 0 ; i < 360 / rotatn_angle ; i++)
+		 {
+		  flag_goes_into_interrupt=0;
+		  motion_set(0x0A);
+		  angle_rotate(rotatn_angle);
+		  motion_set(0x00);		  
+		  send_data(0x00);//send byte to laptop
+
+		  while(!flag_goes_into_interrupt); //Will wait until data is received
+		  
+		  //goes into interrupt
+		  flag_goes_into_interrupt = 0;
+		  first_bit = (data & 0x80)/128;
+		  if(first_bit == 1)
+		  {
+			 	net_angle = (((global_orientatn % 4)*90 + (counter*rotatn_angle)) % 360 );
+				move_towards_ball();
+		  }
+		  while(data != 0x00);
+
+		  _delay_ms(500);
+		  counter++;
+		  /*check pulkits ans
+		  if yes pura loop break*/
+		 }
+ }
+
+
+
 
 int check_obstacles(unsigned int check_value)
 {
@@ -344,11 +432,11 @@ void move_forward(unsigned int distance)
 
 void update_coordinates (int angle)
 {
-
+	
 	net_angle = net_angle + angle;
 	//remember right side positive
-	forward = forward + 200*cos(net_angle*3.14/180);
-	right = right + 200*sin(net_angle*3.14/180);
+	//forward = forward + 200*cos(net_angle);
+	//right = right + 200*sin(net_angle);
 
 }
 
@@ -369,39 +457,12 @@ void rotate_to_centre( int angle )
 }
 
 
-void retrace()
-{
-    if(net_angle >= 0)
-    motion_set(0x05);
-
-    else
-    {
-        motion_set(0x0A);
-        net_angle = net_angle*(-1);
-    }
-
-    angle_rotate(net_angle);
-	_delay_ms(2000);
-	motion_set(0x05);
-
-    double y = (forward - forward_last); 
-	double x = (right - right_last);
-    double z = atan2( y , x );
-	double degrees = ((z / 3.142 + .5)*180);
-	angle_rotate( degrees );
-
-    linear_distance_mm1( sqrt((x*x)+(y*y)) );
-	motion_set(0x0A);
-    angle_rotate(degrees); 
-}
-
-
 void init_devices (void)
 {
  cli();
  port_init();
- 
  adc_init();
+ uart0_init();
  left_position_encoder_interrupt_init();
  right_position_encoder_interrupt_init();
  sei();
@@ -409,62 +470,56 @@ void init_devices (void)
 
 int main()
 { init_devices();
+  //unsigned int forward_count = 0;
+  //unsigned int right_count = 0;
   unsigned int value;
   unsigned char sharp;
 
-
+  unsigned int rotatn_angle = 90;
   unsigned int arena_forward=900,arena_right=900;
   //unsigned int init_forward=forward, init_right=right;
   unsigned int move = 300, side = 300;
   unsigned int left_or_right = 0;
 
-  int pulkit_ans=10;
+  while(1)
+  {
+  	 // forward =0;
+	 // while(((forward + move <= arena_forward) && global_orientatn == 0) || ((forward <= move) && global_orientatn == 2))
+	 while((forward + move <= arena_forward && global_orientatn ==0) || (forward >= move && global_orientatn ==2 ))
+	  {
+		  rotate_360(rotatn_angle);
+		  //motion_set(0x00);
+		  move_forward(move);
+		  motion_set(0x00);
+		  _delay_ms(1000);
+	  }
+	  if(left_or_right == 0)
+	  {
+		  turn_right1();
+		  _delay_ms(1000);
+		  move_forward(side);
+		  motion_set(0x00);
+		  _delay_ms(1000);
+		  turn_right();
+		  // motion_set(0x00);
+		  _delay_ms(1000);
+		  left_or_right = 1;
 
+	  }
+	  else
+	  {
+		  turn_left();
+		  // motion_set(0x00);
+		  _delay_ms(1000);
+		  move_forward(side);
+		  motion_set(0x00);
+		  _delay_ms(1000);
+		  turn_left();
+		  // motion_set(0x00);
+		  _delay_ms(1000);
+		  left_or_right = 0;
+	  }
 
-  int proportionality_factor=10;
-  int reqd_angle;
-  net_angle = (((global_orientatn % 4)*90 + (counter*rotatn_angle)) % 360 );// counter define
-  forward_last = forward;
-  right_last = right;
-  
-  
-  	 pulkit_ans=-5;
-	 //ball found
-	 reqd_angle = pulkit_ans * proportionality_factor;//receive distance from centre of image
-	 rotate_to_centre(reqd_angle);
-	 linear_distance_mm1(200);
-	 update_coordinates(reqd_angle);
-	 _delay_ms(2000);
-	 //update pulkit ans
-
-     pulkit_ans=9;
-	 //ball found
-	 reqd_angle = pulkit_ans * proportionality_factor;//receive distance from centre of image
-	 rotate_to_centre(reqd_angle);
-	 linear_distance_mm1(200);
-	 update_coordinates(reqd_angle);
-	 _delay_ms(2000);
-
-	 pulkit_ans= 3;
-	 //ball found
-	 reqd_angle = pulkit_ans * proportionality_factor;//receive distance from centre of image
-	 rotate_to_centre(reqd_angle);
-	 linear_distance_mm1(200);
-	 update_coordinates(reqd_angle);
-	 _delay_ms(2000);
-
-	 pulkit_ans=-2;
-	 //ball found
-	 reqd_angle = pulkit_ans * proportionality_factor;//receive distance from centre of image
-	 rotate_to_centre(reqd_angle);
-	 linear_distance_mm1(200);
-	 update_coordinates(reqd_angle);
-	 _delay_ms(2000);
-
-     retrace();
-     //forward = forward_last;
-     //right = right_last;
-
-
+	  
 }
-
+}
